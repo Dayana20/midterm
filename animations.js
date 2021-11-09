518bada801bc4994b7f40ecbf0701c82cf5cfecf
Dayana20 <39806222+Dayana20@users.noreply.github.com>
2021-11-08 20:20:45 -0500

    updating

--- animations.js
+++ animations.js
@@ -2,26 +2,11 @@ function endIt() {
     alert("Sorry You're Dead :(");
 }
 
-document.onkeydown = detectKey;
-function detectKey(e) {
-    var posLeft = document.getElementById('myId').offsetLeft;
-    var posTop = document.getElementById('myId').offsetTop;
-    e = e || window.event;
-    if (e.keyCode == '38') {
-        // up arrow
-        document.getElementById('myId').style.marginTop  = (posTop-58)+"px";
-    }
-    else if (e.keyCode == '40') {
-        // down arrow
-        document.getElementById('myId').style.marginTop  = (posTop+58)+"px";
-        // document.getElementById('myId').style.top  =parseInt(document.getElementById('myId'))+5+"px";
-    }
-    else if (e.keyCode == '37') {
-       // left arrow
-        document.getElementById('myId').style.marginLeft  = (posLeft-58)+"px";
-    }
-    else if (e.keyCode == '39') {
-       // right arrow
-        document.getElementById('myId').style.marginLeft  = (posLeft+58)+"px";
-    }
-}
+function endIt2() {
+    setTimeout(function() { 
+        alert("Sorry You're Dead :("); 
+        location.href="index.html";
+    }, 9000);
+    // location.href="index.html";
+    
+} 
